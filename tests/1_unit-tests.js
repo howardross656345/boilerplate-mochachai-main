bb182ad0fe76523b3ffc8b2ae2759d0b4c06ed56
howardross656345 <124522296+howardross656345@users.noreply.github.com>
2023-07-02 03:45:10 +0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,30 +1,30 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+suite('Unit Tests', function() {
+  suite('Basic Assertions', function() {
     // #1
-    test('#isNull, #isNotNull', function () {
+    test('#isNull, #isNotNull', function() {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.isNotNull(1, '1 is not null');
     });
     // #2
-    test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+    test('#isDefined, #isUndefined', function() {
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
-    test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+    test('#isOk, #isNotOk', function() {
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isOk(true, 'true is truthy');
     });
     // #4
-    test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+    test('#isTrue, #isNotTrue', function() {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
